30846b17dc51cb29e90ccf95da7f2f2bbdda53a9
Long Đỗ <20520619@ms.uit.edu.vn>
2022-05-18 21:44:10 +0700

    Update

--- Application/WindowsFormsApp1/Admin.cs
+++ Application/WindowsFormsApp1/Admin.cs
@@ -26,7 +26,7 @@ namespace WindowsFormsApp1
         private void button2_Click(object sender, EventArgs e)
         {
             List_HH p = new List_HH();
-            p.Show(); 
+            p.Show();
         }
     }
 }
