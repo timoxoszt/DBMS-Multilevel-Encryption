72a6024d9a89182bb0aae2b1b80dde27c8d507bf
timoxoszt <20520810@gm.uit.edu.vn>
2022-05-24 08:15:45 +0000

    disable register

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+    'login' => true,
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
