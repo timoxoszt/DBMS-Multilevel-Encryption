dbc9bf4010da60c13db91b763a8ab1b9b162a3b2
timoxoszt <20520810@gm.uit.edu.vn>
2022-06-06 17:55:48 +0000

    ChuShopHang {create|edit|delete} Users && KeToan view Users

--- Backend/app/Http/Controllers/API/UserController.php
+++ Backend/app/Http/Controllers/API/UserController.php
@@ -15,6 +15,33 @@ use DB;
 
 class UserController extends BaseController
 {
+    /*
+        ========= POLICY =========
+        -- Thêm-Sửa-Xóa Orders|Users|Products -- 
+        UserRole: {ChuShopHang}
+        UserType: {Owner}
+        Section: {Order Product User}
+
+        -- Thêm-Sửa-Xóa Orders && Xem Users --
+        UserRole: {KeToan} 
+        UserType: {Manager}
+        Section: {Order Product User}
+
+        -- Thêm-Sửa-Xóa Products --
+        UserRole: {ThuKho} 
+        UserType: {Managerr}
+        Section: {Product}
+
+        -- Xem Orders --
+        UserRole: {NguoiGiaoHang} 
+        UserType: {Employee}
+        Section: {Order}
+
+        -- Xem-Sửa-Xóa {Order || Thông tin cá nhân} của chính mình && Xem Products --
+        UserRole: {NguoiMuaHang}
+        UserType: {Customer}
+        Section: {Product}
+    /*
     /**
      * Display a listing of the resource.
      *
@@ -22,8 +49,13 @@ class UserController extends BaseController
      */
     public function index()
     {
-        $users = User::all();
-        return $this->sendResponse(UserResource::collection($users), 'Users fetched.');
+        $authUser = Auth::user();
+        if((Crypt::decryptString($authUser->user_type) == "Owner" && Crypt::decryptString($authUser->user_role) == "ChuShopHang") || (Crypt::decryptString($authUser->user_type) == "Manager" && Crypt::decryptString($authUser->user_role) == "KeToan")){
+            $users = User::all();
+            return $this->sendResponse(UserResource::collection($users), 'Users fetched.');
+        }else{
+            return $this->sendError('Permission denied.');
+        }       
     }
 
     /**
@@ -55,11 +87,16 @@ class UserController extends BaseController
      */
     public function show($uuid)
     {
-        $user = User::where('uuid', $uuid)->first();
-        if (is_null($user)) {
-            return $this->sendError('User does not exist.');
-        }
-        return $this->sendResponse(new UserResource($user), 'User fetched.');
+        $authUser = Auth::user();
+        if((Crypt::decryptString($authUser->user_type) == "Owner" && Crypt::decryptString($authUser->user_role) == "ChuShopHang") || (Crypt::decryptString($authUser->user_type) == "Manager" && Crypt::decryptString($authUser->user_role) == "KeToan")){
+            $user = User::where('uuid', $uuid)->first();
+            if (is_null($user)) {
+                return $this->sendError('User does not exist.');
+            }
+            return $this->sendResponse(new UserResource($user), 'User fetched.');
+        }else{
+            return $this->sendError('Permission denied.');
+        }  
     }
 
     /**
@@ -82,37 +119,42 @@ class UserController extends BaseController
      */
     public function update(Request $request, $uuid)
     {
-        $user = User::where('uuid', $uuid)->first();
-        if (is_null($user)) {
-            return $this->sendError('User does not exist.');
-        }
-        $input = $request->all();
-        $validator = Validator::make($input, [
-            'email' => 'required|string',
-            'ho_ten' => 'required|string',
-            'sdt' => 'required',
-            'dia_chi' => 'required',
-            'stk' => 'required',
-            'cmnd' => 'required',
-            'ngay_sinh' => 'required',
-        ]);
-        if($validator->fails()){
-            return $this->sendError($validator->errors());       
-        }
-        try{           
-            $user->ho_ten = Crypt::encryptString($input['ho_ten']);
-            $user->email = $input['email'];
-            $user->sdt = Crypt::encryptString($input['sdt']);
-            $user->dia_chi = Crypt::encryptString($input['dia_chi']);
-            $user->stk = Crypt::encryptString($input['stk']);
-            $user->cmnd = Crypt::encryptString($input['cmnd']);
-            $user->ngay_sinh = Crypt::encryptString($input['ngay_sinh']);
-            $user->save();
-
-            return $this->sendResponse(new UserResource($user), 'User updated.');
-        }catch(\Exception $e){
+        $authUser = Auth::user();
+        if((Crypt::decryptString($authUser->user_type) == "Owner" && Crypt::decryptString($authUser->user_role) == "ChuShopHang")){
+            $user = User::where('uuid', $uuid)->first();
+            if (is_null($user)) {
+                return $this->sendError('User does not exist.');
+            }
+            $input = $request->all();
+            $validator = Validator::make($input, [
+                'email' => 'required|string',
+                'ho_ten' => 'required|string',
+                'sdt' => 'required',
+                'dia_chi' => 'required',
+                'stk' => 'required',
+                'cmnd' => 'required',
+                'ngay_sinh' => 'required',
+            ]);
+            if($validator->fails()){
+                return $this->sendError($validator->errors());       
+            }
+            try{           
+                $user->ho_ten = Crypt::encryptString($input['ho_ten']);
+                $user->email = $input['email'];
+                $user->sdt = Crypt::encryptString($input['sdt']);
+                $user->dia_chi = Crypt::encryptString($input['dia_chi']);
+                $user->stk = Crypt::encryptString($input['stk']);
+                $user->cmnd = Crypt::encryptString($input['cmnd']);
+                $user->ngay_sinh = Crypt::encryptString($input['ngay_sinh']);
+                $user->save();
+    
+                return $this->sendResponse(new UserResource($user), 'User updated.');
+            }catch(\Exception $e){
+                return $this->sendError('Permission denied.');
+            };
+        }else{
             return $this->sendError('Permission denied.');
-        }
+        } 
     }
 
     /**
@@ -123,18 +165,23 @@ class UserController extends BaseController
      */
     public function destroy($uuid)
     {        
-        $user = User::where('uuid', $uuid)->first();
+        $authUser = Auth::user();
+        if((Crypt::decryptString($authUser->user_type) == "Owner" && Crypt::decryptString($authUser->user_role) == "ChuShopHang") || (Crypt::decryptString($authUser->user_type) == "Manager" && Crypt::decryptString($authUser->user_role) == "KeToan")){
+            $user = User::where('uuid', $uuid)->first();
 
-        if (is_null($user)) {
+            if (is_null($user)) {
             return $this->sendError('User does not exist.');
-        }
-        try{
-            $user->delete();
-            return $this->sendResponse([], 'User deleted.');
-        }          
-        catch(\Exception $e){
-            return $this->sendError('Unauthorised.');
-        }            
+            }
+            try{
+                $user->delete();
+                return $this->sendResponse([], 'User deleted.');
+            }          
+            catch(\Exception $e){
+                return $this->sendError('Permission denied.');
+            }   
+        }else{
+            return $this->sendError('Permission denied.');
+        }           
     }
 
     public function profile(Request $request)
